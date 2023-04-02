5156bd18b13aacfecb8e632db5cfac0680582a06
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2023-04-02 20:12:15 +0100

    update management script by updating automatically background image

--- scripts/management.js
+++ scripts/management.js
@@ -22,6 +22,8 @@ document.onreadystatechange = function() {
 
 
     } else {
+
+        updateBackground();
        
         //delay for 2500ms
         setTimeout(() => {
@@ -44,7 +46,6 @@ document.onreadystatechange = function() {
                 }, 1000);
              } , 500);
 
-            
         }, 2500);
 
 
@@ -106,9 +107,58 @@ function typingAnimation (id) {
 
   function updateBackground() {
 
-    let backgrounds = [
-
+    let idx = -1,
+    timer = 1000 * 60 * 60, // 1h
+    backgrounds = [
+        "/images/01-Early-Morning.png",
+        "/images/02-Mid-Morning.png",
+        "/images/03-Late-Morning.png",
+        "/images/04-Early-Afternoon.png",
+        "/images/05-Mid-Afternoon.png",
+        "/images/06-Late-Afternoon.png",
+        "/images/07-Early-Evening.png",
+        "/images/08-Mid-Evening.png",
+        "/images/09-Late-Evening.png",
+        "/images/10-Early-Night.png",
+        "/images/11-Mid-Night.png",
+        "/images/12-Late-Night.png"
     ];
+
+    setInterval(() => {
+        let hour = new Date().getHours();
+
+        // morning - 5am - 12pm
+        // afternoon - 12pm - 5pm
+        // evening - 5pm - 9pm
+        // night - 9pm - 4am
+        if(hour >= 5 && hour < 8) {
+            idx = 0;
+        } else if (hour >= 8 && hour < 11) {
+            idx = 1;
+        } else if (hour >= 11 && hour < 12) {
+            idx = 2;
+        } else if (hour >= 12 && hour < 14) {
+            idx = 3;
+        } else if (hour >= 14 && hour < 16) {
+            idx = 4;
+        } else if (hour >= 16 && hour < 17) {
+            idx = 5;
+        } else if (hour >= 17 && hour < 18) {
+            idx = 6;
+        } else if (hour >= 18 && hour < 20) {
+            idx = 7;
+        } else if (hour >= 20 && hour < 22) {
+            idx = 8;
+        } else if (hour >= 22 && hour < 24) {
+            idx = 9;
+        } else if (hour >= 24 && hour < 2) {
+            idx = 10;
+        } else {
+            idx = 11;
+        }
+
+        document.querySelector("body").setAttribute("style", `background-image: url(${backgrounds[idx]})`);
+    }, timer);
 
   }
 
